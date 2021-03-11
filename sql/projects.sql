53e822dec34e38d8af200fe77c31adc51845d419
limitofzero <limitofzero@gmail.com>
2021-03-11 23:49:19 +0300

    feat(): add reusable sql for projects

--- sql/projects.sql
+++ sql/projects.sql
@@ -1,16 +1,11 @@
-create table projects
+create table if not exists projects
 (
-	id uuid default uuid_generate_v4() not null,
-	name varchar(20) not null
+    id uuid default uuid_generate_v4() not null primary key,
+    name varchar(20) not null
 );
 
-create unique index projects_id_uindex
-	on projects (id);
+create unique index if not exists projects_id_uindex
+    on projects (id);
 
-create unique index projects_name_uindex
-	on projects (name);
-
-alter table projects
-	add constraint projects_pk
-		primary key (id);
-
+create unique index if not exists projects_name_uindex
+    on projects (name);
